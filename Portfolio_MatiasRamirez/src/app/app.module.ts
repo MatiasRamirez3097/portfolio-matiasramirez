f1d44b23771facc4fb987bda04b54f6c830b07f5
Matias Ramirez <matiasgr3009@gmail.com>
2022-08-10 11:05:59 -0300

    added about and experience

--- Portfolio_MatiasRamirez/src/app/app.module.ts
+++ Portfolio_MatiasRamirez/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExpComponent } from './components/exp/exp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    AboutComponent,
+    ExpComponent
   ],
   imports: [
     BrowserModule,
